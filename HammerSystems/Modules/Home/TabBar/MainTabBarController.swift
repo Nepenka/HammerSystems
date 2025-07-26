34e4af582886ac22bb48d5f85bbb411b7128cda3
Vladislav Perelygin <perelyginvladislav02@gmail.com>
2025-07-26 11:21:40 +0300

    Исправил баг с входом

--- HammerSystems/Modules/Home/TabBar/MainTabBarController.swift
+++ HammerSystems/Modules/Home/TabBar/MainTabBarController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 
 final class MainTabBarController: UITabBarController {
+    
+    private let showLoginSuccessBanner: Bool
+    
+    init(showLoginSuccessBanner: Bool = false) {
+        self.showLoginSuccessBanner = showLoginSuccessBanner
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabs()
@@ -17,7 +29,7 @@ final class MainTabBarController: UITabBarController {
     }
 
     private func setupTabs() {
-        let menuVC = HomeBuilder.build()
+        let menuVC = HomeBuilder.build(showLoginSuccessBanner: showLoginSuccessBanner)
         menuVC.tabBarItem = UITabBarItem(title: "Меню", image: UIImage(systemName: "list.bullet"), tag: 0)
 
         let contactsVC = ContactBuilder.build()
